12098070d66af0148bb0a6191d03bf7e829849c9
Jacek Pietal <jacek@gamecodehq.com>
2018-07-01 14:52:43 +0200

    updated for single debug and debug with limit

--- README.md
+++ README.md
@@ -20,13 +20,21 @@ a.time('1000ms')
 a.time('0ms')
 
 setTimeout(() => {
-	a.timeEnd('1000ms')
-	console.log(a.debugAll())
-	process.exit(0)
+  a.timeEnd('1000ms')
+  console.log(a.debugAll())
+  process.exit(0)
 }, 1000)
 
 a.timeEnd('0ms')
-console.log(a.debugAll())
+
+// will print both
+a.log(a.debugAll())
+
+// will print this one, always
+a.log(a.debug('1000ms'))
+
+// wont print anything, because it lasted 0ms < 1000ms
+a.log(a.debug('0ms', 1000))
 ```
 
 ## Author
--- source/index.js
+++ source/index.js
@@ -28,22 +28,36 @@ Fluffy.prototype = {
     this.totals[name].time += this.totals[name].lastTime
     this.totals[name].from = null
   },
+  debug (name, minMs) {
+    var total = this.totals[name]
+    if (!minMs || (total.lastTime > minMs)) {
+      return this.formatFn(name)
+    }
+  },
+  formatFn (name) {
+    var total = this.totals[name]
+    if (total.count > 0) {
+      return JSON.stringify({
+        name,
+        last: +total.lastTime.toFixed(2),
+        avg: +(total.time / total.count).toFixed(2),
+        sum: +total.time.toFixed(2)
+      })
+    }
+  },
+  log (value) {
+    if (value) {
+      console.log(value)
+    }
+  },
   filterFn (name) {
     return (this.totals[name].count > 0)
   },
   debugAll () {
     return Object.keys(this.totals)
       .filter(this.filterFn.bind(this))
-      .map((name) => {
-        var total = this.totals[name]
-        return JSON.stringify({
-          name,
-          last: +total.lastTime.toFixed(2),
-          avg: +(total.time / total.count).toFixed(2),
-          sum: +total.time.toFixed(2)
-        })
-      }
-    ).join(', ')
+      .map(this.formatFn.bind(this))
+      .join(', ')
   }
 }
 
