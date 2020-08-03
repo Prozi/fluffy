10890b383c16709a01a031243600f864076d6299
Jacek Pietal <prozi85@gmail.com>
2020-08-03 05:42:14 +0200

    feat: output in json format + bump version

--- source/index.js
+++ source/index.js
@@ -1,76 +1,73 @@
-'use strict'
+"use strict";
 
-var getTime = require('./time').default
+const getTime = require("./time").default;
 
-function Fluffy () {
-  this.totals = {}
+function Fluffy() {
+  this.totals = {};
 }
 
 Fluffy.prototype = {
-  time (name) {
+  time(name) {
     if (!this.totals[name]) {
       this.totals[name] = {
         time: 0,
-        count: 0
-      }
+        count: 0,
+      };
     }
     // for recursive functions
     if (!this.totals[name].from) {
-      this.totals[name].from = getTime()
+      this.totals[name].from = getTime();
     }
   },
-  timeEnd (name) {
+  timeEnd(name) {
     if (!this.totals[name] || !this.totals[name].from) {
-      return
+      return;
     }
-    this.totals[name].count++
-    this.totals[name].lastTime = getTime() - this.totals[name].from
-    this.totals[name].time += this.totals[name].lastTime
-    this.totals[name].from = null
+    this.totals[name].count++;
+    this.totals[name].lastTime = getTime() - this.totals[name].from;
+    this.totals[name].time += this.totals[name].lastTime;
+    this.totals[name].from = null;
   },
-  debug (name, minMs) {
-    var total = this.totals[name]
-    if (!minMs || (total.lastTime > minMs)) {
-      return this.formatFn(name)
-    }
-  },
-  formatFn (name) {
-    var total = this.totals[name]
-    if (total.count > 0) {
-      return JSON.stringify({
-        name,
-        last: +total.lastTime.toFixed(2),
-        avg: +(total.time / total.count).toFixed(2),
-        sum: +total.time.toFixed(2)
-      })
+  debug(name, minMs) {
+    const total = this.totals[name];
+    if (!minMs || total.lastTime > minMs) {
+      return this.formatFn(name);
     }
   },
-  log (value) {
+  formatFn(name) {
+    const total = this.totals[name];
+    return {
+      name,
+      last: total.lastTime,
+      avg: total.count ? total.time / total.count : 0,
+      sum: total.time,
+    };
+  },
+  log(value) {
     if (value) {
-      console.log(value)
+      console.log(value);
     }
   },
-  filterFn (name, minMs) {
+  filterFn(name, minMs) {
     if (!this.totals[name].count) {
-      return false
+      return false;
     }
-    if (minMs && (this.totals[name].lastTime < minMs)) {
-      return false
+    if (minMs && this.totals[name].lastTime < minMs) {
+      return false;
     }
-    return true
+    return true;
   },
-  debugAll (minMs) {
-    var fluffy = this
-    var min = minMs || 0
-    return Object.keys(this.totals)
-      .filter(function (name) {
-        return fluffy.filterFn(name, min)
-      })
-      .map(this.formatFn.bind(this))
-      .join(', ')
-  }
-}
+  debugAll(minMs) {
+    const min = minMs || 0;
+    return Object.keys(this.totals).reduce((acc, stat) => {
+      if (this.filterFn(stat, min)) {
+        acc[stat] = this.formatFn(stat).avg;
+      }
+      return acc;
+    }, {});
+  },
+};
 
-module.exports = Fluffy
+module.exports = Fluffy;
 
-module.exports.default = Fluffy
+module.exports.default = Fluffy;
